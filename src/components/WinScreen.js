b2b90808f827c21cdf47965d606be387d38d689f
Dean <deaneffi1414@gmail.com>
2023-03-14 11:05:25 +0200

    add confetti

--- src/components/WinScreen.js
+++ src/components/WinScreen.js
@@ -1,15 +1,42 @@
-import React from "react"
+import React, { useState }  from "react"
+import Confetti from 'react-confetti'
+import { useEffect } from 'react'
 
 
 
 
 export default function WinScreen(props){
+    const [width,setWidth] = useState(window.innerWidth)
+    const [height,setHeight] = useState(window.innerheight)
+
+
+    useEffect(()=>{
+        window.addEventListener("resize",()=>{
+
+            setWidth(window.innerWidth)
+            setHeight(window.innerheight)
+
+        })
+
+    },[])
 
     return(
         <div className="h-2/3 w-screen p-10 text-bold  text-5xl flex flex-col items-center
-        gap-3  justify-center ">
-            <h2>  You beat the game! 
-            </h2>
+        gap-10  justify-center ">
+
+
+
+
+            <Confetti
+                width={width}
+                height={height}
+                numberOfPieces={500}
+                gravity={0.1}
+                initialVelocityY={10}
+            />
+
+
+            <h2 className="lg:text-6xl bg-gradient-to-r from-emerald-500 via-emerald-600 to-emerald-700 bg-clip-text text-transparent 	"> You beat the game! </h2>
           
 
 
